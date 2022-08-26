137d96dab01976b7fad5216b69f42578285f2ec3
Hannyxxx <hannyacostam@gmail.com>
2022-08-26 00:03:49 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import logo from '../logo.svg';
+const NavBar = () => {
+    return(
+        <div className="navbar-contenedor">
+<img src={logo} className="App-logo" alt="logo" />
+
+<ul ClassName="nav-menu">
+<li><a href="">Inicio</a></li>
+<li><a href="">Diseños</a></li>
+<li><a href="">Quienes Somos?</a></li>
+<li><a href=""className='btn'><button>Contacto</button></a></li>
+</ul>
+
+        </div>
+    )
+}
+
+export default NavBar;
